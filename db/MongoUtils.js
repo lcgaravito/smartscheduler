71db63a595dd17ff259e6949b9be99c334589aab
sebastianvaca99 <js.vacat@uniandes.edu.co>
2020-02-28 13:16:01 -0500

    efs

--- db/MongoUtils.js
+++ db/MongoUtils.js
@@ -2,32 +2,33 @@ const MongoClient = require("mongodb").MongoClient;
 
 function MongoUtils() {
   const mu = {},
-    hostname = "",
-    port = 27017,
-    dbName = "",
-    conName = "";
+    user = //process.env.USER,
+    password = //process.env.PASSWORD,
+    dbName = "smartSchedule",
+    colName = "schedules";
 
   mu.connect = () => {
-    const client = new MongoClient( `mongodb://${hostname}:${port}`, {
+    const client = new MongoClient( `mongodb+srv://${user}:${password}@cluster0-3lhwp.mongodb.net/test?retryWrites=true&w=majority`, {
       useUnifiedTopology: true
     });
     return client.connect();
   };
 
+  mu.schedules={};
+
+  mu.schedules.find = query =>
+    mu.connect.then(client =>{
+      const schedulesCol = client.db(dbName).collection(colName);
+      schedulesCol.insertOne({"name":"Juan"});
+      return schedulesCol 
+        .find(query)
+        .limit(20)
+        .sort({ timestap: -1 })
+        .toArray()
+        .finally(() => client.close());
+    });
   // replace the uri string with your connection string.
-  const uri = MongoClient.connect(uri, function(err, client) {
-    if (err) {
-      console.log("Error occurred while connecting to MongoDB Atlas...\n", err);
-    }
-    console.log("Connected...");
-    // perform actions on the collection object
-    const db = client.db("smartscheduler");
-    const col = db.collection("Users");
-
-    col.insertOne("{test:test}");
-
-    client.close();
-  });
+  return mu;
 }
 
-module.exports = MongoUtils;
+module.exports = MongoUtils();
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require("express");
 var router = express.Router();
+const bd = require("../db/MongoUtils.js");
 
 /* GET home page. */
-router.get("/", function(req, res) {
+router.get("/", function(req, res) 
+{
+  console.log("asdasd");
   res.render("index", { title: "Express" });
+  bd.connect()
+    .then(cliente => {
+      console.log("cliente: ",cliente);
+      
+      const a = cliente.bd("smartScheduler");
+      a.collection("schedules").insertOne({"name": "Juan Pablo"}).then(result => console.log("listo", result));
+    });
+    
 });
+
 
 module.exports = router;
